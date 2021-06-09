189062574eff5520b71a90525344892f44f1d692
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-06-10 06:04:51 +0800

    added led dimmer code

--- exercise-5-led-dimmer/led-dimmer.ino
+++ exercise-5-led-dimmer/led-dimmer.ino
@@ -4,7 +4,7 @@
 *			   will perform an analog read from pin A0
 *              and based on the reading (0 - 1023), 1024 possible 
 *			   values, an analog write will be made to the LED (0-254),
-*			   256 possible values.
+*			   255 possible values.
 * Author: ZAND (Zarex Alvin Daria)
 * History:
 * Date 		Description 		Programmer Name
